5a7b42432f43b785bb778a228edd9c0f22af9651
Snarie <144214499+Snarie@users.noreply.github.com>
2024-10-09 00:42:09 +0200

    Added routes for profiles
    Only show route is available for now.

--- routes.php
+++ routes.php
@@ -2,3 +2,11 @@
 
 $router->get("/", "controllers\HomeController@home");
 $router->get("/about", "controllers\HomeController@about");
+
+//$router->get("/profiles", "controllers\ProfileController@index");
+//$router->get("/profiles/create", "controllers\ProfileController@create");
+//$router->post("profiles", "controllers\ProfileController@store");
+$router->get("/profiles/{id}", "controllers\ProfileController@show");
+//$router->get("/profiles/{id}/edit", "controller\ProfileController@edit");
+//$router->put("/profiles/{id}", "controller\ProfileController@update");
+//$router->delete("/profiles/{id}", "controller\ProfileController@destroy");
