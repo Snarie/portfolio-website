45a8488ed148194829ed40beb93c336c547a3729
Snarie <144214499+Snarie@users.noreply.github.com>
2024-10-30 22:33:03 +0100

    Created some links to the footer
    Includes a link to github and a mail to link
    Added a link to the cv to download

--- layout/footer.php
+++ layout/footer.php
@@ -2,11 +2,32 @@
 
 </section>
 
-<section>
-	<p>p in div stukkie 1</p>
-	<p>p in div stukkei 2</p>
+<section class="grid gap-10">
+    <article>
+        <p class="f-24">Pages</p>
+        <a class="footer-link" href="/">Home</a>
+        <a class="footer-link" href="/about">About</a>
+        <a class="footer-link" href="/projects">Projects</a>
+    </article>
+    <article>
+        <p class="f-24">Latest
+            <?php
+
+            ?>
+            ?>
+        </p>
+    </article>
 </section>
 
 <section>
-    <img src="/public/images/twitter-icon-dark.png">
+    <p>Contact me</p>
+    <br>
+    <a class="footer-icon" href="https://github.com/Snarie"><img src="/public/images/github-icon.png" alt=""></a>
+    <a class="footer-icon" href="mailto:barryvandenberg@gmail.com"><img src="/public/images/mail-icon.png" alt=""></a>
+    <br><br>
+    <a class="footer-link" href="/public/CV-Resume.pdf">Download cv</a>
+</section>
+
+<section style="grid-column: span 3">
+    <br><h3>wwwwwwwwww wwwwwwwwwww wwwwwwwwwwww wwwwwwwwwwww wwwwwwwwwwww wwwwwwwwww wwwwwwwwwww wwwwwwwwww wwwwwwwww</h3>
 </section>
