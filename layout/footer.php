b2522440ad2f1838373e6c0e1a7e3ffe29b60470
Snarie <144214499+Snarie@users.noreply.github.com>
2024-10-30 23:22:49 +0100

    Updated positioning to place responsibility closer to the articles

--- layout/footer.php
+++ layout/footer.php
@@ -1,34 +1,32 @@
-<section>
-
-</section>
-
-<section class="grid gap-10 column-2">
-    <article>
-        <p class="f-24">Pages</p>
+<section class="grid gap-10 column-3">
+    <article class="grid gap-10 column-2">
+        <p class="f-24" style="grid-column: span 2;">Pages</p>
         <a class="footer-link" href="/">Home</a>
         <a class="footer-link" href="/about">About</a>
         <a class="footer-link" href="/projects">Projects</a>
     </article>
-    <article>
-        <p class="f-24">Latest Projects</p>
-            <?php
-                $latestProjects = \App\Models\Project::latestProjects();
-                foreach($latestProjects as $project) {
-                    echo "<a class=\"footer-link\" href=\"/projects/$project->id\">$project->name</a>";
-                }
-            ?>
+
+    <article class="grid gap-10 column-2 ">
+        <p class="f-24" style="grid-column: span 2">Latest Projects</p>
+	    <?php
+	    $latestProjects = \App\Models\Project::latestProjects(4);
+	    foreach($latestProjects as $project) {
+		    echo "<a class=\"footer-link\" href=\"/projects/$project->id\">$project->name</a>";
+	    }
+	    ?>
     </article>
+
+    <article style="display: grid">
+        <p class="f-24">Contact me</p>
+        <div>
+            <a class="footer-icon" href="https://github.com/Snarie"><img src="/public/images/github-icon.png" alt=""></a>
+            <a class="footer-icon" href="mailto:barryvandenberg@gmail.com"><img src="/public/images/mail-icon.png" alt=""></a>
+        </div>
+        <a class="footer-link" href="/public/CV-Resume.pdf">Download Resume</a>
+    </article>
+
 </section>
 
 <section>
-    <p>Contact me</p>
-    <br>
-    <a class="footer-icon" href="https://github.com/Snarie"><img src="/public/images/github-icon.png" alt=""></a>
-    <a class="footer-icon" href="mailto:barryvandenberg@gmail.com"><img src="/public/images/mail-icon.png" alt=""></a>
-    <br><br>
-    <a class="footer-link" href="/public/CV-Resume.pdf">Download cv</a>
-</section>
-
-<section style="grid-column: span 3">
     <br><p>© Copyright 2024 Barry van den Berg</p>
 </section>
