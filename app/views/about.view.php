f53e08017850c73b898410ec22188b05c7ef6c66
Snarie <144214499+Snarie@users.noreply.github.com>
2024-10-31 03:19:37 +0100

    Added some text to the about view

---
+++ app/views/about.view.php
@@ -0,0 +1,23 @@
+<section class="title-container grid gap-30">
+	<article class="mw-1024px">
+		<h1 class="all-upper">About me</h1>
+		<h2>Here you will find information about me</h2>
+
+	</article>
+</section>
+
+<section class="mw-1600px">
+    <article class="mw-1024px">
+        <h3>
+            Hello, my name is Barry van den Berg, i'm a student at Windesheim for Software Development in Almere.
+            <br><br>
+            I'm passionate about programming, math, which keep me busy most of the day.
+            I spend a lot of my free time learning more about my interests to deepen my knowledge and skills.
+            <br><br>
+            When i'm not busy with learning, i enjoy gaming or going out with friends.
+            <br><br>
+            I've been coding since i was 12, and am still improving every day, i love the create the back-end of websites or programs, while trying to improve my understanding of creation a good UI/UX.
+            
+        </h3>
+    </article>
+</section>
